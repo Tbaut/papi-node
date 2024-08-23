359e42f10cfc302e37a6f0dfc16303df00a3096b
Thibaut Sardan <github@thib.top>
2024-08-23 18:50:09 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { dot, people, alep } from '@polkadot-api/descriptors';
 import { createClient, type TypedApi } from 'polkadot-api';
 import { getWsProvider } from 'polkadot-api/ws-provider/node';
 
-const pplWs = getWsProvider('wss://polkadot-people-rpc.polkadot.io');
+// const pplWs = getWsProvider('wss://polkadot-people-rpc.polkadot.io');
 const dotWs = getWsProvider('wss://rpc.ibp.network/polkadot');
-const alepWs = getWsProvider('wss://aleph-zero.api.onfinality.io/public-ws');
+// const alepWs = getWsProvider('wss://aleph-zero.api.onfinality.io/public-ws');
 
 // Connect to the polkadot relay chain.
-const pplClient = createClient(pplWs);
+// const pplClient = createClient(pplWs);
 const dotClient = createClient(dotWs);
-const alepClient = createClient(alepWs);
+// const alepClient = createClient(alepWs);
 
 // With the `client`, you can get information such as subscribing to the last
 // block to get the latest hash:
@@ -17,9 +17,9 @@ const alepClient = createClient(alepWs);
 //   console.log(finalizedBlock.number, finalizedBlock.hash)
 // );
 
-const peopleApi = pplClient.getTypedApi(people);
+// const peopleApi = pplClient.getTypedApi(people);
 const dotApi = dotClient.getTypedApi(dot);
-const alepApi = alepClient.getTypedApi(alep);
+// const alepApi = alepClient.getTypedApi(alep);
 
 const alepAddress = '5DxrqFfMPP6pUigSkSqKgHtgdwe8gtjGRsBNkW9xs9YfdHv8';
 const polkadotAddress = '12doHFjPjPngNvZCWX4WeF4rkLFJ5LmmEyDvPGQ2C1aPppwy';
@@ -40,5 +40,5 @@ const getIdentity = (
 };
 
 getIdentity(dotApi);
-getIdentity(peopleApi);
-getIdentity(alepApi);
+// getIdentity(peopleApi);
+// getIdentity(alepApi);
