265fb19b227bd6b90aacda54e976859840e8e302
Thibaut Sardan <github@thib.top>
2024-09-19 11:26:13 +0100

    dryrun large calldata

--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,67 @@
-import { dot, people, alep } from '@polkadot-api/descriptors';
-import { createClient, type TypedApi } from 'polkadot-api';
-import { withPolkadotSdkCompat } from 'polkadot-api/polkadot-sdk-compat';
+import { wnd } from '@polkadot-api/descriptors';
+import { Binary, createClient } from 'polkadot-api';
 import { getWsProvider } from 'polkadot-api/ws-provider/node';
+import { sr25519CreateDerive } from '@polkadot-labs/hdkd';
+import {
+  DEV_PHRASE,
+  entropyToMiniSecret,
+  mnemonicToEntropy,
+} from '@polkadot-labs/hdkd-helpers';
+import { getPolkadotSigner } from 'polkadot-api/signer';
 
-const pplWs = getWsProvider('wss://polkadot-people-rpc.polkadot.io');
-const pplClient = createClient(pplWs);
-const peopleApi = pplClient.getTypedApi(people);
+const main = async () => {
+  // ALICE ACCOUNT
+  const derive = sr25519CreateDerive(
+    entropyToMiniSecret(mnemonicToEntropy(DEV_PHRASE)),
+  );
+  const aliceKeyPair = derive('//Bob');
+  const aliceSigner = getPolkadotSigner(
+    aliceKeyPair.publicKey,
+    'Sr25519',
+    aliceKeyPair.sign,
+  );
 
-const alepWs = getWsProvider('wss://aleph-zero.api.onfinality.io/public-ws');
-const alepClient = createClient(withPolkadotSdkCompat(alepWs));
-const alepApi = alepClient.getTypedApi(alep);
+  // CLIENT AND API
+  const client = createClient(
+    getWsProvider('wss://rpc-test-network-1.novasamatech.org'),
+  );
+  const api = client.getTypedApi(wnd);
 
-// const dotWs = getWsProvider('wss://rpc.ibp.network/polkadot');
-// const dotClient = createClient(dotWs);
-// const dotApi = dotClient.getTypedApi(dot);
+  // create the call from some call-data
+  const tx = await api.txFromCallData(
+    Binary.fromHex(
+      '0x0402001cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c001cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c0b00a0724e1809',
+    ),
+  );
+  // create the signed extrinsic
+  const signedTx = await tx.sign(aliceSigner);
+  // see what the result of this extrinsic would be against the current best-block
+  const dryRunResult = await api.apis.BlockBuilder.apply_extrinsic(
+    Binary.fromOpaqueHex(signedTx),
+    { at: 'best' },
+  );
+  // `dryRunResult` is a strongly typed object, so if `success` is false, then
+  // the value will have a strongly typed enum with the reason why it didn't succeed
+  console.log('dryRunResult', dryRunResult);
+  // In your case it would print:
+  // {
+  //  success: false,
+  //  value: {
+  //    type: "Invalid",
+  //    value: {
+  //      type: "ExhaustsResources",
+  //      value: undefined,
+  //    },
+  //  },
+  // }
 
-// With the `client`, you can get information such as subscribing to the last
-// block to get the latest hash:
-// pplClient.finalizedBlock$.subscribe((finalizedBlock) =>
-//   console.log(finalizedBlock.number, finalizedBlock.hash)
-// );
-
-const alepAddress = '5Gjy5yh1ZtHFDkqptN2UqyMMqTjPSZSH8nJcz3oTA6Z6Dtmy';
-const polkadotAddress = '12doHFjPjPngNvZCWX4WeF4rkLFJ5LmmEyDvPGQ2C1aPppwy';
-const pplAddress = '13Mg5VGLpLSHKfKhEzicruy7GBqY5MNcRiCyzHK9HvZti9rf';
-
-// const getIdentity = async ({
-//   api,
-//   type,
-//   address,
-// }:
-//    {
-//       type: 'people';
-//       api: TypedApi<typeof people>;
-//       address: string;
-//     }
-//   | {
-//       type: 'alep';
-//       api: TypedApi<typeof alep>;
-//       address: string;
-//     }) => {
-//   const val = await api.query.Identity.IdentityOf.getValue(address);
-
-//   if (!val) {
-//     console.log('no val');
-//     return;
-//   }
-
-//   if (type === 'people') {
-//     const [res] = val;
-//     console.log('ppl', res.info);
-//     return;
-//   }
-
-//   if (type === 'alep') {
-//     const { info } = val;
-//     console.log('alep', info);
-//     return;
-//   }
-// };
-
-type IdentityParams =
-  | {
-      type: 'people';
-      api: TypedApi<typeof people>;
-      address: string;
-    }
-  | {
-      type: 'alep';
-      api: TypedApi<typeof alep>;
-      address: string;
-    };
-
-const getIdentity = async ({ type, api, address }: IdentityParams) => {
-  if (type === 'people') {
-    const val = await api.query.Identity.IdentityOf.getValue(address);
-
-    if (!val) {
-      console.log('no val');
-      return;
-    }
-    const [res] = val; // api is inferred to be TypedApi<typeof people>
-    console.log('ppl', res.info.display.value?.asText());
-    return;
-  }
-
-  if (type === 'alep') {
-    const val = await api.query.Identity.IdentityOf.getValue(address);
-
-    if (!val) {
-      console.log('no val');
-      return;
-    }
-
-    const { info } = val; // api is inferred to be TypedApi<typeof alep>
-    console.log('alep', info.display.value?.asText());
-    return;
+  // Therefore, you could first check whether the dryRun worked before
+  // broadcasting the transaction
+  if (dryRunResult.success) {
+    const submitResult = await client.submit(signedTx);
+  } else {
+    // some error handling logic
   }
 };
 
-// getIdentity(dotApi);
-getIdentity({ api: peopleApi, type: 'people', address: pplAddress });
-getIdentity({ api: alepApi, type: 'alep', address: alepAddress });
+main();
