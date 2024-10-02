569026ce1c3942254bb483f415df18d0882a900a
Thibaut Sardan <github@thib.top>
2024-10-02 18:25:45 +0100

    fix: with decoding internal calls

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { pas } from '@polkadot-api/descriptors';
 import { createClient, Transaction } from 'polkadot-api';
 import { getWsProvider } from 'polkadot-api/ws-provider/node';
-import { JSONprint } from './utils';
+import { hashFromTx, JSONprint } from './utils';
 import {
   getDynamicBuilder,
   getLookupFn,
@@ -90,8 +90,71 @@ const getExtDecoderAt = async (blockHash?: string) => {
   });
 };
 
+const getEncodedCallFromDecodedTx = (
+  decodedTx: any,
+  compatibilityToken: any,
+) => {
+  const batch = pasApi.tx.Utility.batch({
+    calls: [decodedTx],
+  }).getEncodedData(compatibilityToken);
+
+  // we slice the 3 bytes from utility batch
+  return batch.asHex().replace('0x', '').slice(6);
+};
+
+const getMultisigInfo = async (
+  call: Transaction<any, any, any, any>['decodedCall'],
+) => {
+  const compatibilityToken = await pasApi.compatibilityToken;
+
+  const result: any[] = [];
+
+  console.log('----for call', JSONprint(call));
+
+  const getCallResults = (
+    call: Transaction<any, any, any, any>['decodedCall'],
+  ) => {
+    if (call.type === 'Multisig') {
+      if (call.value.type === 'as_multi') {
+        const callDatawithout0x = getEncodedCallFromDecodedTx(
+          call.value.value.call,
+          compatibilityToken,
+        );
+        const callData = `0x${callDatawithout0x}`;
+        const hash = hashFromTx(callData);
+        // console.log('----- callData', callData);
+        // console.log('----- hash', hash);
+        result.push({
+          name: `${call.value.value.call.type}.${call.value.value.call.value.type}`,
+          hash,
+          callData,
+        });
+      } else if (call.value.type === 'approve_as_multi') {
+        result.push({
+          name: 'Unknown call',
+          hash: call.value.value.call_hash,
+          callData: undefined,
+        });
+      }
+    } else {
+      if (!!call.value.value.call) {
+        getCallResults(call.value.value.call);
+      }
+
+      if (!!call.value.value.calls) {
+        call.value.value.calls.forEach((c: any) => getCallResults(c));
+      }
+    }
+  };
+
+  getCallResults(call);
+  console.log('result', JSONprint(result));
+  return result;
+};
+
 const main = async () => {
-  const BLOCK_HEIGHT = 14004;
+  // const BLOCK_HEIGHT = 14004;
+  const BLOCK_HEIGHT = 3134939;
 
   const blockHash = (
     await pasClient._request('archive_unstable_hashByHeight', [BLOCK_HEIGHT])
@@ -106,27 +169,28 @@ const main = async () => {
     blockHash,
   ])) as HexString[];
 
-  console.log('blockHash', JSONprint(blockHash));
-  console.log('body', JSONprint(body));
+  // console.log('blockHash', JSONprint(blockHash));
+  // console.log('body', JSONprint(body));
 
   const decoder = await getExtDecoderAt(blockHash);
-  body.forEach((tx) => {
-    console.log('-----------------------------');
-    console.log(JSONprint(decoder(tx)));
-  });
+  // body.forEach((tx) => {
+  //   console.log('-----------------------------');
+  //   console.log(JSONprint(decoder(tx)));
+  // });
   const promises = body.map((tx) => {
     const decodedExtrinsic = decoder(tx);
     const toDecode = decodedExtrinsic.signed
       ? (decodedExtrinsic.body as any).callData
       : decodedExtrinsic.body;
-    console.log('-----------------------------');
-    console.log(JSONprint(decodedExtrinsic));
+    // console.log('-----------------------------');
+    // console.log(JSONprint(decodedExtrinsic));
     return pasApi.txFromCallData(Binary.fromHex(toDecode));
   });
 
   Promise.all(promises).then((txs) => {
-    console.log('-------------decoded');
-    console.log('txs', JSONprint(txs));
+    // console.log('-------------decoded');
+    // console.log('txs', JSONprint(txs));
+    txs.map((tx) => getMultisigInfo(tx.decodedCall));
   });
 };
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,9 @@
+import { Blake2256 } from '@polkadot-api/substrate-bindings';
+import { toHex, fromHex } from '@polkadot-api/utils';
+import { HexString } from 'polkadot-api';
+
 export const JSONprint = (e: unknown) =>
-    JSON.stringify(e, (_, v) => (typeof v === 'bigint' ? v.toString() : v), 4);
+  JSON.stringify(e, (_, v) => (typeof v === 'bigint' ? v.toString() : v), 4);
+
+export const hashFromTx = (tx: HexString) =>
+  toHex(Blake2256(fromHex(tx))) as HexString;
